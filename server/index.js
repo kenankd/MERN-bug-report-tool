1554347025741ef5de9372b45b6fb40e2aee8b5c
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-07 10:53:29 +0200

    added body-parser

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 const app = express();
 const PORT = 4000;
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 });
+
