7d22b6716cc8a884fd9dbea9f2dac32c4e9c3dfb
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-07 14:47:17 +0200

    user registration with password hash

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,19 @@
-export const register = (req,res) => {
-    res.status(201).send('Test');
+import bcrypt from 'bcrypt'
+
+import {SALT_ROUNDS} from '../constants.js'
+import User from '../models/User.model.js'
+
+export const register = async (req,res) => {
+    const{password,...data} = req.body;
+    try{
+    const hashedPassword = await bcrypt.hash(password,SALT_ROUNDS);
+    const userToSave = new User({...data,password:hashedPassword});
+    const result = await userToSave.save();
+    res.status(201).send("User created!");
+    }
+    catch(e){
+        res.status(500).send("Could not create user");
+    }
 }
 
 export const login = (req,res) => {
