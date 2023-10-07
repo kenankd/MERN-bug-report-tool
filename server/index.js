2f7e6091d16981e2f55692997ffddc97f19bf197
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-07 12:28:39 +0200

    route import

--- server/index.js
+++ server/index.js
@@ -5,11 +5,13 @@ import authRoutes from '../routes/auth.routes.js'
 const app = express();
 const PORT = 4000;
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
 mongoose.connect('mongodb://127.0.0.1:27017/bug-report').then(
     () => console.log('Connected!')
 );
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use('auth',authRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
