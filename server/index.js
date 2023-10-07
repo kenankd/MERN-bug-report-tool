78e079c776b1c9fecafe66a0158528d05b3929e7
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-07 11:17:12 +0200

    mongodb connected with mongoose

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 const app = express();
 const PORT = 4000;
 
 app.use(bodyParser.urlencoded({extended:false}));
-
 app.use(bodyParser.json());
+mongoose.connect('mongodb://127.0.0.1:27017/bug-report').then(
+    () => console.log('Connected!')
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
