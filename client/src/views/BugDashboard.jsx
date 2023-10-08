a94a92dbb6a71eaa552045257af73e9f5d431fa9
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-08 15:41:58 +0200

    bugs fetched

--- client/src/views/BugDashboard.jsx
+++ client/src/views/BugDashboard.jsx
@@ -1,12 +1,17 @@
 import { Card, CardContent, Box, CardActions, Button, TextField, Typography } from '@mui/material';
 import BugReportOutlinedIcon from '@mui/icons-material/BugReportOutlined';
 import {useEffect, useState} from 'react';
+import axiosInstance from '../axios-instance.js'
 
 const BugDashboard = () => {
     const [bugs,setBugs] = useState([]);
-
+    const fetchBugs = async () => {
+        const result = await axiosInstance.get('/bugs/');
+        console.log(result.data);
+        setBugs(result.data);
+    }
     useEffect(()=>{
-        const result = await axios.get('/');
+        fetchBugs();
     },[])
     return (
         <Box>
@@ -20,6 +25,16 @@ const BugDashboard = () => {
                    alignItems: "center"}}>
                 <Typography>Bug app</Typography>
                 <BugReportOutlinedIcon />
+            </Box>
+            <Box sx={{display:"flex",padding:"20px"}}>
+                {bugs.map((bug)=>(
+                    <Card sx={{maxWidth:"250px"}}>
+                        <CardContent>
+                            <Typography>{bug?.title}</Typography>
+                            <Typography>{bug?.steps}</Typography>
+                        </CardContent>
+                    </Card>
+                ))}
             </Box>
         </Box>
     )
