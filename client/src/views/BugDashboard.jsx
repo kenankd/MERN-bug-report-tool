507dbcbbcc0a09cf7cc830d1da2c08f39d45bbc7
kenankd <kdizdarevi1@etf.unsa.ba>
2023-10-08 15:52:02 +0200

    IIFE bug fetch

--- client/src/views/BugDashboard.jsx
+++ client/src/views/BugDashboard.jsx
@@ -1,34 +1,37 @@
 import { Card, CardContent, Box, CardActions, Button, TextField, Typography } from '@mui/material';
 import BugReportOutlinedIcon from '@mui/icons-material/BugReportOutlined';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import axiosInstance from '../axios-instance.js'
 
 const BugDashboard = () => {
-    const [bugs,setBugs] = useState([]);
-    const fetchBugs = async () => {
-        const result = await axiosInstance.get('/bugs/');
-        console.log(result.data);
-        setBugs(result.data);
-    }
-    useEffect(()=>{
-        fetchBugs();
-    },[])
+    const [bugs, setBugs] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const result = await axiosInstance.get('/bugs/');
+            console.log(result.data);
+            setBugs(result.data);
+        })()
+    }, []);
+    useEffect(() => {
+        console.log(bugs);
+    }, [bugs]);
     return (
         <Box>
             <Box sx={{
-                 backgroundColor: "black",
-                  color: "white",
-                   height: "40px",
-                   textAlign: "center",
-                   display: "flex",
-                   justifyContent: "center",
-                   alignItems: "center"}}>
+                backgroundColor: "black",
+                color: "white",
+                height: "40px",
+                textAlign: "center",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center"
+            }}>
                 <Typography>Bug app</Typography>
                 <BugReportOutlinedIcon />
             </Box>
-            <Box sx={{display:"flex",padding:"20px"}}>
-                {bugs.map((bug)=>(
-                    <Card sx={{maxWidth:"250px"}}>
+            <Box sx={{ display: "flex", padding: "20px" }}>
+                {bugs.map((bug) => (
+                    <Card sx={{ maxWidth: "250px" }}>
                         <CardContent>
                             <Typography>{bug?.title}</Typography>
                             <Typography>{bug?.steps}</Typography>
